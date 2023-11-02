a64eb994c883b1caf7db3cc29ab4de4d89c37962
Luchi <luchichronicles@gmail.com>
2023-11-02 10:24:57 -0700

    initial comit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Facts Site with a dar/light mode toggler
+
+A simple react app with two components - Main and Nav - that toggles light and dark mode on click.
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+export default function App() {
+  const [darkMode, setDarkMode] = useState(false)
+
+  function toggleDarkMode(){
+    setDarkMode(prev => !prev)
+  }
+  return (
+    <div className='container'>
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
+    </div>
+  );
+}
